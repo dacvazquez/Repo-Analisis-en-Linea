4425f2dac632b95315161efec575de7ecbcd0c21
Daniel Capote <daniel.capote2002@gmail.com>
2025-03-18 20:13:35 -0400

    crated an analizer_funtion.py file to modulize the proyect a bit and migrated the functions on the app.py to that file, the load_analizers() method initiates when tthe pages load

---
+++ analizer_functions.py
@@ -0,0 +1,100 @@
+# Define sentiment prediction function
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+def sentiment_analisys(text,sentiment_analyzer):
+    # Preprocess text
+    text_words=[]
+    for word in text.split(' '):
+        if word.startswith('@') and len(word)>1:
+            word='@user'
+        elif word.startswith('http'):
+            word='http'
+        text_words.append(word)
+    full_text= ' '.join(text_words)
+    
+    labels = ['Negativo', 'Neutro', 'Positivo']
+    colores = ['#FF0000', '#808080', '#00FF00'] 
+    
+    sentimiento=sentiment_analyzer.predict(full_text)
+    #ejemplo de salida: AnalyzerOutput(output=POS, probas={POS: 0.857, NEU: 0.103, NEG: 0.040})
+    
+    probs = sentimiento.probas
+    sent = sentimiento.output
+    
+    probs_array = np.array(list(probs.values()))
+    max_prob=np.max(probs_array)
+        
+    
+    fig = go.Figure(data=[go.Pie(labels=labels, values=probs_array, 
+                             textinfo='label+percent', textposition='inside',
+                             marker_colors=colores)])
+   
+        # Dar una respuestas
+    if sent == 'NEG':
+        color_response="**<font color='red'>Negativo</font>**"
+    elif sent == 'POS':
+        color_response="**<font color='green'>Positivo</font>**"
+    else:
+        color_response="**<font color='grey'>Neutro</font>**"
+    resp=f"El texto es: {color_response} con una probabilidad de {max_prob*100:.2f}%."
+
+    return resp, fig
+    
+def hate_analisys(text, hate_analizer):
+    # Preprocess text
+    text_words=[]
+    for word in text.split(' '):
+        if word.startswith('@') and len(word)>1:
+            word='@user'
+        elif word.startswith('http'):
+            word='http'
+        text_words.append(word)
+    full_text= ' '.join(text_words)
+    
+    hate=hate_analizer.predict(full_text)
+    
+    #ejemplo de salida: AnalyzerOutput(output=[], probas={hateful: 0.022, targeted: 0.009, aggressive: 0.018})
+    
+    probs = hate.probas
+    hate = hate.output
+    
+    #labels = ['Odioso', 'Agresivo', 'Dirigido', 'No Odioso']
+    
+    probs_array = np.array(list(probs.values()))
+    max_prob=np.max(probs_array)
+        
+    # Etiquetas y valores
+    traduccion = {
+    'hateful': 'Odioso',
+    'aggressive': 'Agresivo',
+    'targeted': 'Directo'
+    }
+    df = pd.DataFrame({
+    'Etiqueta': [traduccion[k] for k in probs.keys()],
+    'Intensidad': list(probs.values())
+    })
+    #df = pd.DataFrame(list(probs.items()), columns=['Etiqueta', 'Probabilidad'])
+    # Crear el gráfico de barras
+    fig = px.bar(df, x='Etiqueta', y='Intensidad', color='Etiqueta', color_discrete_map={
+    'Odioso': '#FF0000',  # Rojo
+    'Agresivo': '#FF9900',  # Naranja
+    'Dirigido': '#FFFF00'  # Amarillo
+    })
+        # Dar una respuestas
+    resp=''
+    for clasificacion in hate:
+        if clasificacion == 'hateful':
+            color_response = "**<font color='red'>Odioso</font>**"
+        elif clasificacion == 'aggressive':
+            color_response = "**<font color='orange'>Agresivo</font>**"
+        elif clasificacion == 'targeted':
+            color_response = "**<font color='yellow'>Dirigido</font>**"
+        resp+=f"El texto es: {color_response} con una intensidad de {probs[clasificacion]*100:.2f}%. <br>"
+    if resp=='':
+        resp=f"El texto es: **<font color='green'>No odioso</font>**"
+    return resp, fig
+    
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 from pysentimiento import create_analyzer
-
+from analizer_functions import sentiment_analisys, hate_analisys
 
 @st.cache_resource
 def load_analizers():
@@ -23,115 +23,17 @@ def load_analizers():
 
     return sentiment_analyzer, hate_analizer
 
-# Define sentiment prediction function
-def sentiment_analisys(text):
-    # Preprocess text
-    text_words=[]
-    for word in text.split(' '):
-        if word.startswith('@') and len(word)>1:
-            word='@user'
-        elif word.startswith('http'):
-            word='http'
-        text_words.append(word)
-    full_text= ' '.join(text_words)
-    
-    sentiment_analyzer=load_analizers()[0]
-    
-    labels = ['Negativo', 'Neutro', 'Positivo']
-    colores = ['#FF0000', '#808080', '#00FF00'] 
-    
-    sentimiento=sentiment_analyzer.predict(full_text)
-    #ejemplo de salida: AnalyzerOutput(output=POS, probas={POS: 0.857, NEU: 0.103, NEG: 0.040})
-    
-    probs = sentimiento.probas
-    sent = sentimiento.output
-    
-    probs_array = np.array(list(probs.values()))
-    max_prob=np.max(probs_array)
-        
-    
-    fig = go.Figure(data=[go.Pie(labels=labels, values=probs_array, 
-                             textinfo='label+percent', textposition='inside',
-                             marker_colors=colores)])
-   
-        # Dar una respuestas
-    if sent == 'NEG':
-        color_response="**<font color='red'>Negativo</font>**"
-    elif sent == 'POS':
-        color_response="**<font color='green'>Positivo</font>**"
-    else:
-        color_response="**<font color='grey'>Neutro</font>**"
-    resp=f"El texto es: {color_response} con una probabilidad de {max_prob*100:.2f}%."
-
-    return resp, fig
-    
-def hate_analisys(text):
-    # Preprocess text
-    text_words=[]
-    for word in text.split(' '):
-        if word.startswith('@') and len(word)>1:
-            word='@user'
-        elif word.startswith('http'):
-            word='http'
-        text_words.append(word)
-    full_text= ' '.join(text_words)
-    
-    hate_analizer=load_analizers()[1]
-    
-    hate=hate_analizer.predict(full_text)
-    
-    #ejemplo de salida: AnalyzerOutput(output=[], probas={hateful: 0.022, targeted: 0.009, aggressive: 0.018})
-    
-    probs = hate.probas
-    hate = hate.output
-    
-    #labels = ['Odioso', 'Agresivo', 'Dirigido', 'No Odioso']
-    
-    probs_array = np.array(list(probs.values()))
-    max_prob=np.max(probs_array)
-        
-    # Etiquetas y valores
-    traduccion = {
-    'hateful': 'Odioso',
-    'aggressive': 'Agresivo',
-    'targeted': 'Dirigido'
-    }
-    df = pd.DataFrame({
-    'Etiqueta': [traduccion[k] for k in probs.keys()],
-    'Intensidad': list(probs.values())
-    })
-    #df = pd.DataFrame(list(probs.items()), columns=['Etiqueta', 'Probabilidad'])
-    # Crear el gráfico de barras
-    fig = px.bar(df, x='Etiqueta', y='Intensidad', color='Etiqueta', color_discrete_map={
-    'Odioso': '#FF0000',  # Rojo
-    'Agresivo': '#FF9900',  # Naranja
-    'Dirigido': '#FFFF00'  # Amarillo
-    })
-        # Dar una respuestas
-    resp=''
-    for clasificacion in hate:
-        if clasificacion == 'hateful':
-            color_response = "**<font color='red'>Odioso</font>**"
-        elif clasificacion == 'aggressive':
-            color_response = "**<font color='orange'>Agresivo</font>**"
-        elif clasificacion == 'targeted':
-            color_response = "**<font color='yellow'>Dirigido</font>**"
-        resp+=f"El texto es: {color_response} con una intensidad de {probs[clasificacion]*100:.2f}%. <br>"
-    if resp=='':
-        resp=f"El texto es: **<font color='green'>No odioso</font>**"
-    return resp, fig
-    
-
 # Main app logic
 def main():
     st.title("Procesamiento de Texto en Redes Sociales")
     # User input
+    sentiment_analyzer, hate_analizer=load_analizers()
     option = st.selectbox("Elige una opción", ["Análisis de Sentimiento", "Detección de Odio"])
     st.markdown("<br>", unsafe_allow_html=True)
     if option == "Análisis de Sentimiento":
         text_input = st.text_area("Escriba el texto para realizar el analisis de sentimiento")
         if st.button("Analizar Sentimiento"):
-            resp, fig=sentiment_analisys(text_input)
+            resp, fig=sentiment_analisys(text_input, sentiment_analyzer)
             st.markdown(resp, unsafe_allow_html=True)
             with st.container():
                 st.write("### Distribución de Sentimientos")
@@ -139,7 +41,7 @@ def main():
     elif option == "Detección de Odio":             
         text_input = st.text_area("Escriba el texto para relizar detección de Odio") 
         if st.button("Analizar Odio"):
-            resp, fig=hate_analisys(text_input) 
+            resp, fig=hate_analisys(text_input, hate_analizer) 
             #st.success("Buscar los demas", icon="👍")
             st.markdown(resp, unsafe_allow_html=True)
             with st.container():
