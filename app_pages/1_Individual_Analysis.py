7d962ed914f92ed503530404ea324a19fdba7147
Daniel Capote <daniel.capote2002@gmail.com>
2025-04-24 02:04:07 -0400

    switched the predefined text of the inputs on Individual_Analysis.py like: text_input = st.text_area("Texto a realizar análisis de sentimiento") on line 15 for the Sentiment Analysis

--- app_pages/1_Individual_Analysis.py
+++ app_pages/1_Individual_Analysis.py
@@ -13,7 +13,7 @@ def main():
     st.markdown("<br>", unsafe_allow_html=True)
     
     if option == "Análisis de Sentimiento":
-        text_input = st.text_area("Escriba el texto para realizar el análisis de sentimiento")
+        text_input = st.text_area("Texto a realizar análisis de sentimiento")
         if st.button("Analizar Sentimiento"):
             with st.spinner('Analizando sentimiento...'):
                 sentiment, sent_probs, fig = sentiment_analisys(text_input, sentiment_analyzer)
@@ -31,7 +31,7 @@ def main():
                 st.write("### Distribución de Sentimientos")
                 st.plotly_chart(fig, use_container_width=True)
     elif option == "Detección de Odio":             
-        text_input = st.text_area("Escriba el texto para realizar detección de Odio") 
+        text_input = st.text_area("Texto a realizar detección de Odio") 
         if st.button("Analizar Odio"):
             with st.spinner('Analizando discurso de odio...'):
                 hate, hate_probs, fig = hate_analisys(text_input, hate_analizer) 
@@ -44,7 +44,7 @@ def main():
                 elif clasification == 'aggressive':
                     hate_response = cr.agresivo
                 elif clasification == 'targeted':
-                    hate_response = cr.odioso
+                    hate_response = cr.dirigido
                 elif clasification == 'none':
                     hate_response = cr.no_odioso
                 resp+=f"El texto es: {hate_response} con una precisión de {hate_probs[clasification]*100:.2f}%. <br>"
@@ -52,7 +52,7 @@ def main():
                 resp=f"El texto es: **<font color='green'>No odioso</font>**"
             st.markdown(resp, unsafe_allow_html=True)
             with st.container():
-                st.write("### Distribución de Sentimientos")
+                st.write("### Metricas de Odio")
                 st.plotly_chart(fig, use_container_width=True)
 
 
