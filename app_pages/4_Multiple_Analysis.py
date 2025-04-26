e397958636767a736e5a62dbb13e8c9caae77b9d
Daniel Capote <daniel.capote2002@gmail.com>
2025-04-26 17:35:17 -0400

    Fixed the download and upload problem, now the df drops the first column of the csv and then loads the df

--- app_pages/4_Multiple_Analysis.py
+++ app_pages/4_Multiple_Analysis.py
@@ -89,7 +89,7 @@ def main():
                 'Odio': [hateful],
                 'Agresividad': [aggressive],
                 'Objetivismo': [targeted]
-            })
+            }, index=None)
             st.session_state.analysis_df = pd.concat([st.session_state.analysis_df, new_row], ignore_index=True)
             st.session_state.show_toast = "Texto analizado y añadido correctamente 👍"
             st.rerun()
@@ -100,24 +100,28 @@ def main():
     if not st.session_state.analysis_df.empty:
         st.subheader("Resultados del Análisis")
         st.write(st.session_state.analysis_df)
-        
+              
     # Opciones para importar
     st.subheader("Importar Datos")
-    uploaded_file = st.file_uploader("Sube un archivo CSV", type=['csv'])
-    if uploaded_file is not None:
+    uploaded_file = st.file_uploader("Sube un archivo CSV", type=['csv'], help="Solo se aceptan archivos en el mismo formato que los que se descargan desde la app (descargar en el boton de la parte superior derecha de la tabla)")
+    
+    if uploaded_file is not None and 'last_uploaded_file' not in st.session_state:
         try:
-            df = pd.read_csv(uploaded_file)
-            required_columns = ['Texto', 'Análisis de Sentimiento', 'Odio (hateful)', 'Agresividad (aggressive)', 'Objetivismo (targeted)']
+            df = pd.read_csv(uploaded_file, index_col=False)
+            # Streamlit guarda la primera columna del indice 
+            df = df.drop(df.columns[0],axis=1)
+            required_columns = ['Texto', 'Análisis de Sentimiento', 'Odio', 'Agresividad', 'Objetivismo']
             if all(col in df.columns for col in required_columns):
                 st.session_state.analysis_df = df
                 st.session_state.show_toast = "Archivo importado correctamente 👍"
+                st.session_state.last_uploaded_file = uploaded_file.name
                 st.rerun()
             else:
                 st.error(f"El archivo debe contener las columnas: {', '.join(required_columns)}")
         except Exception as e:
             st.error(f"Error al importar el archivo: {str(e)}")
-
-    # Opción para limpiar el DataFrame
+    
+    # Resetear el estado del archivo cuando se limpia
     if st.button("Limpiar Todos los Resultados :wastebasket:"):
         st.session_state.analysis_df = pd.DataFrame(columns=[
             'Texto', 
@@ -127,6 +131,8 @@ def main():
             'Objetivismo'
         ])
         st.session_state.last_analysis_results = None
+        if 'last_uploaded_file' in st.session_state:
+            del st.session_state.last_uploaded_file
         st.session_state.show_toast = "Resultados limpiados correctamente"
         st.rerun()
 
